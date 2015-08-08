6dbba137502e6e4626ecde3964dc8a5df9ae6569
Greg Winiarski <grzewin@gmail.com>
2015-08-08 09:31:27 +0200

    Custom Slugs Snippet

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 WPAdverts Classifieds Plugin - Snippets
 ========================
 
-This is official library of useful code snippets for [http://wpadverts.com/](WPAdverts - WordPress Classifieds Plugin), which modify default WPAdverts behavior in some way.
+This is official library of useful code snippets for [http://wpadverts.com/](WPAdverts - WordPress Classifieds Plugin), the codes here modify default WPAdverts behavior in some way.
 
 Most of the codes here will never make it into default WPAdverts version mainly because they provide some functionality that only handful of users will find useful.
 
@@ -13,3 +13,7 @@ Most of the codes here will never make it into default WPAdverts version mainly 
 4. Activate the plugin from the WordPress plugins page.
 
 [Need more help installing plugins?](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+## Notes
+
+The code snippet are provided here for free without any guarantee.
---
+++ custom-slugs/custom-slugs.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Plugin Name: Customize Adverts slugs.
+ * Plugin URI: http://wpadverts.com/
+ * Description: This code snippet/plugin explains how to customize Adverts slugs.
+ * Author: Greg Winiarski
+ */
+
+add_action("adverts_post_type", "customize_adverts_post_type");
+add_action("adverts_register_taxonomy", "customize_adverts_taxonomy");
+
+/**
+ * Changes ads url from "advert" to "classified"
+ * 
+ * This will basically change URL from for example 
+ * http://example.com/advert/hello-world/ to http://example.com/classified/hello-world/
+ * 
+ * Note you can use this function to further customize Adverts Custom Post Type the
+ * $args argument is "$args" used in register_post_type function.  In other words
+ * this function (or rather "customize_post_type" filter) gives you an opportunity
+ * to customize adverts Custom Post Type parameters before 
+ * register_post_type function will be called.
+ * 
+ * @see register_post_type()
+ * @link https://codex.wordpress.org/Function_Reference/register_post_type
+ * 
+ * @param array $args Custom Post Type init params
+ * @return array
+ */
+function customize_adverts_post_type( $args ) {
+   if(!isset($args["rewrite"])) {
+      $args["rewrite"] = array();
+   }
+   
+   $args["rewrite"]["slug"] = "classified";
+   return $args;
+}
+
+/**
+ * Customizes Adverts Categories slug
+ * 
+ * This functions changes adverts categories slug from for example
+ * http://example.com/advert-category/toys/ to http://example.com/ad-category/toys/
+ * 
+ * Note you can use this function to further customize Adverts Taxonomy
+ * $args argument is "$args" used in register_taxonomy function. In other words
+ * this function (or rather "customize_adverts_taxonomy" filter) gives you an opportunity
+ * to customize adverts_category taxonomy parameters before register_taxonomy function will be called.
+ * 
+ * @see register_taxonomy()
+ * @link https://codex.wordpress.org/Function_Reference/register_taxonomy
+ * 
+ * @param array $args Parameters that will passed to register_taxonomy function
+ * @return array
+ */
+function customize_adverts_taxonomy( $args ) {
+   if(!isset($args["rewrite"])) {
+      $args["rewrite"] = array();
+   }
+   
+   $args["rewrite"]["slug"] = "ad-category";
+   return $args;
+}
