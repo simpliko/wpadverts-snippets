29b24043354e44c9f5c8c9735af83b5337f4bfc4
Greg Winiarski <grzewin@gmail.com>
2016-03-08 12:11:16 +0100

    Search by category

---
+++ search-by-category/search-by-category.php
@@ -0,0 +1,88 @@
+<?php
+/**
+Plugin Name: WPAdverts Snippets - Search by category
+Version: 1.0
+Author: Greg Winiarski
+Description: Adds categories dropdown input to [adverts_list] search bar.
+*/
+
+// The code below you can paste in your theme functions.php or create
+// new plugin and paste the code there.
+
+add_filter( 'adverts_form_load', 'search_by_category_form_load' );
+add_filter( 'adverts_list_query', 'search_by_category_query' );
+
+/**
+ * Adds category dropdown into search form in [adverts_list].
+ * 
+ * @param array $form Search form scheme
+ * @return array Customized search form scheme 
+ */
+function search_by_category_form_load( $form ) {
+    
+    if( $form['name'] != 'search' ) {
+        return $form;
+    }
+
+    $form['field'][] = array(
+        "name" => "advert_category",
+        "type" => "adverts_field_select",
+        "order" => 20,
+        "label" => __("Category", "adverts"),
+        "max_choices" => 10,
+        "options" => array(),
+        "options_callback" => "adverts_taxonomies",
+        "meta" => array(
+            "search_group" => "visible",
+            "search_type" => "full" 
+        )
+    );
+    
+    add_action( "wp_footer", "search_by_category_footer" );
+    
+    return $form;
+}
+
+/**
+ * Adds tax_query param to WP_Query
+ * 
+ * The tax_query is added only if it is in $_GET['advert_category']
+ * 
+ * @param array $args WP_Query args
+ * @return array Modified WP_Query args
+ */
+function search_by_category_query( $args ) {
+    
+    if( ! adverts_request( "advert_category" ) ) {
+        return $args;
+    }
+    
+    $args["tax_query"] = array(
+        array(
+            'taxonomy' => 'advert_category',
+            'field'    => 'term_id',
+            'terms'    => adverts_request( "advert_category" ),
+        ),
+    );
+    
+    return $args;
+}
+
+/**
+ * Display some additional styling in footer
+ * 
+ * This code should display in your theme footer to add some nice styling
+ * to the category dropdown.
+ * 
+ * return void
+ */
+function search_by_category_footer() {
+    ?>
+    <style type="text/css">
+    .advert-input.advert-input-type-full {
+       width: 100% !important;
+       margin: 1em 0 0 0 !important;
+    }
+    </style>
+    <?php
+}
