5e65d663cf2389960caffc12b82a390704e0b334
Greg Winiarski <grzewin@gmail.com>
2019-01-09 09:52:55 +0100

    support for contact form

--- contact-info-click/contact-info-click.js
+++ contact-info-click/contact-info-click.js
@@ -3,4 +3,8 @@ jQuery(function($) {
         $( ".adverts-button.adverts-show-contact").click();
         $( ".adverts-button.adverts-show-contact").hide();
     }
+    if( $( ".adverts-button.adverts-show-contact-form").length > 0 ) {
+        $( ".adverts-button.adverts-show-contact-form").click();
+        $( ".adverts-button.adverts-show-contact-form").hide();
+    }
 });
