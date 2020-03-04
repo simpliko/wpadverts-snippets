2934f8360541e4501d8709621083b50487374b6f
Greg Winiarski <grzewin@gmail.com>
2020-03-04 13:39:16 +0100

    Global file limit added

--- limit-file-uploads/limit-file-uploads.php
+++ limit-file-uploads/limit-file-uploads.php
@@ -52,6 +52,16 @@ function limit_file_uploads( $form ) {
                 "max" => "4MB"     // maximum file size
             )
         );
+        
+        // Set minimum and maximum total files upload size.
+        // Note. this is a limit for a whole gallery field.
+        $form["field"][$key]["validator"][] = array(
+            "name" => "upload_size_global",
+            "params" => array( 
+                "min" => null,     // minimum files size
+                "max" => null      // maximum files size
+            )
+        );
 
         // Set allowed file types
         // The "allowed" param accepts only: image, video and audio.
@@ -78,4 +88,51 @@ function limit_file_uploads( $form ) {
     }
 
     return $form;
+}
+
+add_action( "init", function() {
+    adverts_form_add_validator("upload_size_global", array(
+        "callback" => "limit_file_uploads_size_global",
+        "label" => "",
+        "params" => array(),
+        "default_error" => __( "The total file size it too big.", "wpadverts" ),
+        "validate_empty" => false
+    ));
+    
+} );
+
+function limit_file_uploads_size_global( $file, $params ) {
+    if ( !isset($file["name"]) || !isset($file["size"]) ) {
+        return true;
+    }
+
+    $max_size = 25*pow(1024,2); // 25MB
+    
+    $size = $file["size"];
+    
+    $post_id = intval( adverts_request( "post_id" ) );
+    
+    if( $post_id === 0 ) {
+        $post_id = intval( adverts_request( "_post_id" ) );
+    } 
+    
+    if( $post_id === 0 ) {
+        $post_id = intval( adverts_request( "advert_id" ) );
+    }
+    
+    if( $post_id > 0) {
+        $atts = get_posts( array(
+            "post_parent" => $post_id,
+            "post_type" => "attachment",
+        ) );
+        foreach( $atts as $att ) {
+            $size += filesize( get_attached_file( $att->ID ) );
+        }
+    }
+    
+    if( $size >= $max_size ) {
+        return "invalid";
+    }
+    
+    return true;
 }
